a6cb11f8b2b2708bba2add1d4221f344762e64c9
Your Name <you@example.com>
2024-05-09 21:57:54 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='font-lato text-3xl'> Welcome </h1>
+      
+      
+    </>
+  )
+}
+
+export default App
