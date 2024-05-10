6288af6cc4dec1cf4690abd542e5585a3b18f0da
Your Name <you@example.com>
2024-05-10 15:16:40 +0900

    404 page ok

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,26 +1,35 @@
-import { Link, useRouteError } from "react-router-dom";
 
+import { Link } from 'react-router-dom';
+import img1 from '/404.jpg'
 
 
 const NotFoundPage = () => {
-    const error = useRouteError();
-    console.log(error);
+    
     return (
         <>
-       
- <section className="flex items-center h-full sm:p-16 dark:bg-gray-50 dark:text-gray-800">
-	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-40 h-40 dark:text-gray-400">
-			<path fill="currentColor" d="M256,16C123.452,16,16,123.452,16,256S123.452,496,256,496,496,388.548,496,256,388.548,16,256,16ZM403.078,403.078a207.253,207.253,0,1,1,44.589-66.125A207.332,207.332,0,0,1,403.078,403.078Z"></path>
-			<rect width="176" height="32" x="168" y="320" fill="currentColor"></rect>
-			<polygon fill="currentColor" points="210.63 228.042 186.588 206.671 207.958 182.63 184.042 161.37 162.671 185.412 138.63 164.042 117.37 187.958 141.412 209.329 120.042 233.37 143.958 254.63 165.329 230.588 189.37 251.958 210.63 228.042"></polygon>
-			<polygon fill="currentColor" points="383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63"></polygon>
-		</svg>
-		<p  className="text-3xl text-teal-700 font-bold">Sorry </p>
-		<p className="text-3xl text-red-600">Looks like our services are currently offline</p>
-		<a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to <Link to="/" className='fond-bold link link-hover text-xl text-green-700 font-bold'> Homepage</Link> </a>
-	</div>
+       <section className="bg-white dark:bg-gray-900 ">
+    <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+        <div className="wf-ull lg:w-1/2">
+            <p className="text-sm font-medium text-blue-500 dark:text-blue-400"> error</p>
+            <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
+            <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for does not exist.Here are some helpful links:</p>
+
+            <div className=" items-center mt-6 gap-x-3">
+                
+                <Link to={'/'} ><button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-orange-400 rounded-lg shrink-0 sm:w-auto hover:bg-yellow-600 dark:hover:bg-green-500 dark:bg-green-600 font-bold">
+                    Take me home
+                </button></Link>
+            </div>
+        </div>
+		
+		<div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
+			<img src={img1} alt="" className="w-full max-w-lg lg:mx-auto"/>
+		</div>
+
+        
+    </div>
 </section>
+ 
         </>
     );
 };
